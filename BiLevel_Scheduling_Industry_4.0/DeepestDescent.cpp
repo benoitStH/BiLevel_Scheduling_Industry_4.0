90d0083f0b0ca44a4f148461b3be29f906978545
benoitStH <95411545+benoitStH@users.noreply.github.com>
2024-01-28 16:05:34 +0100

    add : overwrite of the method solve() in DeepestDescent class
    Added the overriden method of solve().
    It currently return the initial solution generated by the 'generateur' attribute.

--- BiLevel_Scheduling_Industry_4.0/DeepestDescent.cpp
+++ BiLevel_Scheduling_Industry_4.0/DeepestDescent.cpp
@@ -3,6 +3,12 @@
 void DeepestDescent::solve()
 {
 	Solution solution_init = generateur.generateInitialSolution(*instance);
+	Solution best_solution = Solution(solution_init);
+
+	// Heuristic
 
 
+
+	// We save the best solution found
+	setSolution(new Solution(best_solution));
 }
