a958141cc3607da7c6598fc28b0f113cf27a6ac1
benoitStH <95411545+benoitStH@users.noreply.github.com>
2024-01-30 19:17:09 +0100

    add : list of best solution from Follower
    Adding a list of 'Solution' objects. Each one is the best solution found by the Follower. The Leader will decide which one is best.
    
    TODO : an operator to compare two solutions and decide which one is best.

--- BiLevel_Scheduling_Industry_4.0/DeepestDescent.cpp
+++ BiLevel_Scheduling_Industry_4.0/DeepestDescent.cpp
@@ -2,10 +2,11 @@
 
 void DeepestDescent::solve()
 {
-	std::vector<Job> jobs = instance->getListJobs();
-	std::vector<Job> chosenJobs;
+	std::vector<Job> jobs = instance->getListJobs();  // All available jobs
+	std::vector<Job> chosenJobs; // Jobs selected by the Leader
+	std::vector<Solution> follower_solutions;  // list of solutions provided by the follower for each set of jobs given by the Leader
 
-	// Leader's Heuristic
+	// Leader's selects the n among N jobs with the largest processing times
 	std::sort(jobs.begin(), jobs.end(), std::greater<Job>());
 	std::vector<Job>::iterator i = jobs.begin();
 
@@ -20,11 +21,9 @@ void DeepestDescent::solve()
 		i++;
 	}
 
-
 	// Follower's Heuristic
 	Solution solution_init = generateur.generateInitialSolution(chosenJobs, *instance);
 	Solution best_solution = Solution(solution_init);
-
 
 	// We save the best solution found
 	setSolution(new Solution(best_solution));
