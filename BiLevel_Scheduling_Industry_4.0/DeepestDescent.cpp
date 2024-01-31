4a21d5dab446d1e377f88a5eb43e0073ea309f86
benoitStH <95411545+benoitStH@users.noreply.github.com>
2024-01-31 01:48:35 +0100

    add : applying first possible swap on a solution
    The solve() method checks if a job can be swapped with another one in the same bloc and swap them if possible.

--- BiLevel_Scheduling_Industry_4.0/DeepestDescent.cpp
+++ BiLevel_Scheduling_Industry_4.0/DeepestDescent.cpp
@@ -46,10 +46,78 @@ void DeepestDescent::solve()
 	}
 
 	// Follower's Heuristic
-	solution_init = generateur.generateInitialSolution(chosenJobs, *instance);
+	solution_init = Solution(generateur.generateInitialSolution(chosenJobs, *instance));
 	best_solution = Solution(solution_init);
+	std::cout << "Solution Initiale : " << std::endl;
+	best_solution.print();
 
-	std::cout << best_solution;
+	// Swapping jobs between High speed machines
+	unsigned int nbHighSpeedBlocs = solution_init.getMaxNumberOfHighSpeedBlocs();
+
+	/*
+	* We try to perform a swap between two machines for each bloc k
+	* If a swap happened at least once, we try again. Otherwise, we've reached an optimum.
+	*/
+	
+	bool end;
+	do
+	{
+		end = true; // No swap has been performed
+
+		// For each bloc k
+		for (unsigned int k = 0; k < nbHighSpeedBlocs; k++)
+		{
+			// if a swap is possible, do it
+			std::vector<unsigned int> swapableMachines = solution_init.possibleSwapVHighSpeed(k);
+			if (swapableMachines.size() != 0)
+			{
+				solution_init.swapV(swapableMachines, k);
+				end = false;
+			}
+		}
+
+		// if the new solution is better, save it
+		if (solution_init < best_solution)
+		{
+			best_solution = Solution(solution_init);
+		}
+
+	} while (end == false);
+
+
+	// Swapping jobs between Low speed machines
+	unsigned int nbLowSpeedBlocs = solution_init.getMaxNumberOfLowSpeedBlocs();
+
+	/*
+	* The same logic is applied to low speed machines
+	*/
+	do
+	{
+		end = true; // No swap has been performed
+
+		// For each bloc k
+		for (unsigned int k = 0; k < nbLowSpeedBlocs; k++)
+		{
+			// if a swap is possible, do it
+			std::vector<unsigned int> swapableMachines = solution_init.possibleSwapVLowSpeed(k);
+			if (swapableMachines.size() != 0)
+			{
+				solution_init.swapV(swapableMachines, k);
+				end = false;
+			}
+		}
+
+		// if the new solution is better, save it
+		if (solution_init < best_solution)
+		{
+			best_solution = Solution(solution_init);
+		}
+
+	} while (end == false);
+	
+	// The best solution found is added to the list of best solutions
+	std::cout << "Meilleure Solution" << std::endl;
+	best_solution.print();
 	follower_solutions.push_back(best_solution);
 
 	// We save the best solution found which has the lowest score
