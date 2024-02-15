8487978d24b7e2e691b288b3ce9f9dfe8c833716
benoitStH <95411545+benoitStH@users.noreply.github.com>
2024-02-15 12:09:04 +0100

    add : New Selection Rule for the Leader (SPT_EDD_CONST_LATENESS)
    This rule will sort jobs by increasing processing time and earliest due date.
    Assuming we only have one machine with a speed equal to 1, each job's lateness is calculated.
    The jobs with a non positive lateness are selected.
    If there is still an amount of jobs to select, those with the smallest positive lateness are selected until we reach the number of jobs to select.

--- BiLevel_Scheduling_Industry_4.0/LSortRule.h
+++ BiLevel_Scheduling_Industry_4.0/LSortRule.h
@@ -8,7 +8,9 @@ namespace sortRule {
 	const unsigned int RandomRule = 0;
 	const unsigned int LPTRULE = 1;
 	const unsigned int LWPTRULE = 2;
-	const unsigned int InvEDDRule = 3;
+	const unsigned int InvEDDRULE = 3;
+	const unsigned int SPT_EDD_CONST_LATENESS = 4;
+	const unsigned int SPT_EDD_VAR_LATENESS = 5;
 }
 
 class LSortRule : public ILeaderSelectRule
@@ -33,8 +35,16 @@ public:
 			ruleName = "LWPTRule";
 			break;
 
-		case sortRule::InvEDDRule:
+		case sortRule::InvEDDRULE:
 			ruleName = "invEDDRule";
+			break;
+			
+		case sortRule::SPT_EDD_CONST_LATENESS:
+			ruleName = "SPT_EDD_WithConstantLateness";
+			break;
+			
+		case sortRule::SPT_EDD_VAR_LATENESS:
+			ruleName = "SPT_EDD_WithVariableLateness";
 			break;
 
 		default:
@@ -66,7 +76,15 @@ public:
 			return LWPTRule(instance);
 			break;
 
-		case sortRule::InvEDDRule:
+		case sortRule::InvEDDRULE:
+			return std::vector<Job>();
+			break;
+
+		case sortRule::SPT_EDD_CONST_LATENESS:
+			return SptEddConstLateness(instance);
+			break;
+
+		case sortRule::SPT_EDD_VAR_LATENESS:
 			return std::vector<Job>();
 			break;
 
@@ -167,6 +185,82 @@ private:
 		}
 
 		return chosenJobs;
+	}
+
+	/*
+	 * Method which will select a subset of n jobs among the list of Jobs given
+	 * The Selection criterion is the Lateness of jobs ordered by increasing processing time.
+	 * @param instance An instance with the list of jobs and the number of jobs to select
+	 * @return a list of selected jobs
+	 */
+	std::vector<Job> SptEddConstLateness(const Instance& instance)
+	{
+		std::vector<Job> jobs = instance.getListJobs();  // List of jobs
+		std::vector<Job> chosenJobs; // Jobs selected by the Leader's Rule
+
+		std::vector<float> lateness; // lateness of all jobs
+		float cumulatedEndTime = 0;
+
+		// Sorting jobs by increasing processing time and earliest due date
+		std::sort(jobs.begin(), jobs.end(), std::less<Job>());
+
+		// Calculating each job's lateness
+		for (const Job& job : jobs)
+		{
+			cumulatedEndTime += job.getPi();
+
+			lateness.push_back(cumulatedEndTime - job.getDi());
+		}
+
+		unsigned int i = 0;
+
+		// We select jobs with a non positive lateness
+		// until we reach the number of jobs to select or either all jobs are selected or none can be selected
+		while (i != jobs.size())
+		{
+			if (chosenJobs.size() >= instance.getNbToSelectJob())
+			{
+				break;
+			}
+
+			if (lateness[i] <= 0)
+			{
+				chosenJobs.push_back(jobs[i]);
+			}
+			i++;
+		}
+
+		// While there is still an amount of jobs to select
+		while (chosenJobs.size() < instance.getNbToSelectJob())
+		{
+			// We assume the first job has the smallest positive lateness
+			unsigned int min_i = 0;
+			float min_lateness = lateness[min_i];
+
+			// We compare with the other jobs
+			for (i = 1; i < jobs.size(); i++)
+			{
+				// If one of the other job has a smaller positive lateness, we save its index number and its lateness
+				// We also save the job if min_lateness is currently non positive
+				if ((lateness[i] < min_lateness && lateness[i] > 0) || min_lateness <= 0)
+				{
+					min_i = i;
+					min_lateness = lateness[min_i];
+				}
+			}
+
+			// After that, if we found a job with the smallest positive lateness, We select it
+			if (min_lateness > 0)
+			{
+				// Setting its lateness to zero to ensure it won't be selected again
+				lateness[min_i] = 0;
+				chosenJobs.push_back(jobs[min_i]);
+			}
+		}
+
+		return chosenJobs;
+
+
 	}
 };
 
